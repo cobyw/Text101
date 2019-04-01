1a7946c492161ac750680472f0cbc47252c06e2f
Coby <cobyw09@gmail.com>
2019-03-31 23:15:23 -0700

    Adding my assets folder

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    [SerializeField] Text option1;
+    [SerializeField] Text option2;
+    [SerializeField] Text option3;
+
+    State currentState;
+
+    // Use this for initialization
+    void Start () {
+        currentState = startingState;
+        UpdateText();
+	}
+
+    private void UpdateText()
+    {
+        textComponent.text = currentState.GetStateStory();
+        option1.text = currentState.GetOptionOne();
+        option2.text = currentState.GetOptionTwo();
+        option3.text = currentState.GetOptionThree();
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        //using var because the function returns the specific type of variable so I don't have to tell C# what it is. Thats pretty cool.
+        var nextStates = currentState.GetNextStates();
+        var length = nextStates.Length;
+        if (Input.GetKeyDown(KeyCode.Alpha1) && length >= 1)
+        {
+            currentState = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && length >= 2)
+        {
+            currentState = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && length >= 3)
+        {
+            currentState = nextStates[2];
+        }
+        UpdateText();
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject {
+    //Story Text
+   [TextArea(10,14)] [SerializeField] string storyText;
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    //First Option
+   [TextArea(3, 5)] [SerializeField] string optionOne;
+    public string GetOptionOne()
+    {
+        return optionOne;
+    }
+    //Second Option
+    [TextArea(3, 5)] [SerializeField] string optionTwo;
+    public string GetOptionTwo()
+    {
+        return optionTwo;
+    }
+    //Last Option
+    [TextArea(3, 5)] [SerializeField] string optionThree;
+    public string GetOptionThree()
+    {
+        return optionThree;
+    }
+    [SerializeField] State[] nextStates;
+    //These are the states that are available from this state
+    public State[] GetNextStates ()
+    {
+        return nextStates;
+    }
+
+}
